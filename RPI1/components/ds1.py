4f85766cd4a7fd176da9cf23f96544ec94c25ac7
filipvidakovic <filip.vidakovic.04@gmail.com>
2025-12-21 00:26:01 +0100

    Add real sensor code, add event driven loop for sensor

--- RPI1/components/ds1.py
+++ RPI1/components/ds1.py
@@ -10,17 +10,25 @@ def ds1_callback(door_open, timestamp):
 
 
 def run_ds1(settings, threads, stop_event):
-        if settings['simulated']:
-            print("Starting ds1 simulator")
-            ds1_thread = threading.Thread(target = run_ds1_simulator, args=(2, ds1_callback, stop_event))
-            ds1_thread.start()
-            threads.append(ds1_thread)
-            print("Ds1 simulator started")
-        else:
-            from sensors.ds1 import run_ds1_loop, DS1
-            print("Starting ds1 loop")
-            ds1 = DS1(settings['pin'])
-            ds1_thread = threading.Thread(target=run_ds1_loop, args=(ds1, 2, ds1_callback, stop_event))
-            ds1_thread.start()
-            threads.append(ds1_thread)
-            print("Ds1 loop started")
+    if settings['simulated']:
+        print("Starting ds1 simulator")
+        ds1_thread = threading.Thread(
+            target=run_ds1_simulator,
+            args=(ds1_callback, stop_event),
+            daemon=True
+        )
+        ds1_thread.start()
+        threads.append(ds1_thread)
+        print("Ds1 simulator started")
+    else:
+        from sensors.ds1 import run_ds1_loop, DS1
+        print("Starting ds1 loop")
+        ds1 = DS1(settings['pin'])
+        ds1_thread = threading.Thread(
+            target=run_ds1_loop,
+            args=(ds1, 2, ds1_callback, stop_event),
+            daemon=True
+        )
+        ds1_thread.start()
+        threads.append(ds1_thread)
+        print("Ds1 loop started")
--- RPI1/sensors/ds1.py
+++ RPI1/sensors/ds1.py
@@ -1,9 +1,40 @@
-import RPi.GPIO as GPIO
 import time
+import RPi.GPIO as GPIO
 
 
-class DS1(object):
-    pass
+class DS1:
+    def __init__(self, pin):
+        self.pin = pin
 
-def run_ds1_loop():
-    pass
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    def read(self):
+        return GPIO.input(self.pin) == GPIO.HIGH
+
+    def cleanup(self):
+        GPIO.cleanup(self.pin)
+
+def run_ds1_loop(ds1, callback, stop_event, debounce_ms=200):
+
+    def gpio_callback(channel):
+        if stop_event.is_set():
+            return
+
+        door_open = ds1.read()
+        timestamp = time.time()
+        callback(door_open, timestamp)
+
+    GPIO.add_event_detect(
+        ds1.pin,
+        GPIO.BOTH,
+        callback=gpio_callback,
+        bouncetime=debounce_ms
+    )
+
+    try:
+        while not stop_event.is_set():
+            time.sleep(0.1)
+    finally:
+        GPIO.remove_event_detect(ds1.pin)
+        ds1.cleanup()
