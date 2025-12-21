a3ed21d785e00cd90ec483bc1df510d8e8e19e68
filipvidakovic <filip.vidakovic.04@gmail.com>
2025-12-21 01:48:29 +0100

    Add led bulb

--- RPI1/components/ds1.py
+++ RPI1/components/ds1.py
@@ -2,11 +2,17 @@ from simulators.ds1 import run_ds1_simulator
 import threading
 import time
 
+door_led = None
+
 def ds1_callback(door_open, timestamp):
+    global door_led
     t = time.localtime(timestamp)
     print("="*20)
     print(f"Timestamp: {time.strftime('%H:%M:%S', t)}")
     print(f"Door Open: {'Yes' if door_open else 'No'}")
+    print(f"LED bulb: {'On' if door_open else 'Off'}")
+    if door_led:
+        door_led.set_state(door_open)
 
 
 def run_ds1(settings, threads, stop_event):
@@ -22,6 +28,9 @@ def run_ds1(settings, threads, stop_event):
         print("Ds1 simulator started")
     else:
         from sensors.ds1 import run_ds1_loop, DS1
+        from sensors.dl import DoorLED
+        global door_led
+        door_led = DoorLED(settings['led_pin'])
         print("Starting ds1 loop")
         ds1 = DS1(settings['pin'])
         ds1_thread = threading.Thread(
